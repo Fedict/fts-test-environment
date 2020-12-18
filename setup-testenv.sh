4c270d21a4432866eea7b0361dd5375988986d79
Wouter Verhelst <wouter.verhelst@zetes.com>
2020-12-18 13:12:45 +0200

    Drop IDP etc from setup-testenv.sh, too

--- setup-testenv.sh
+++ setup-testenv.sh
@@ -56,21 +56,13 @@ oc new-project --display-name="BOSA Trust Services" bosa-trust-services
 status "Creating ImageStreams and builds..."
 oc process -f builds-streams.yaml | oc apply -f -
 status "Setting up secrets..."
-oc create secret generic softhsm-tokens --from-file=softhsm-tokens.tgz
 oc create secret generic softhsm-tokens-esealing --from-file=softhsm-tokens-esealing.tgz
 sleep 1
 status "Loading images..."
 oc start-build --from-dir=$(pwd) mvn-dockers
-oc start-build --from-dir=$(pwd)/IDP tomcat-oraclejdk
 oc start-build --from-dir=$(pwd) squid
 oc start-build --from-dir=$(pwd)/esealing mvn-esealing
-oc start-build --from-dir=$(pwd)/GUI-IDP guiidp
 oc start-build --from-dir=$(pwd)/GUI-sign guisign
-while [ -z "$(oc get -o json build/tomcat-oraclejdk-1 | jq '.status.completionTimestamp')" ]; do
-	echo "tomcat build not ready yet. Waiting..."
-	sleep 10
-done
-oc start-build --from-dir=$(pwd)/IDP mvn-idp
 oc start-build --from-dir=$(pwd)/sign-validation mvn-signvalidation
 oc create -f configmaps.yaml
 
@@ -80,10 +72,6 @@ cp -a GUI-sign/public/config gui-config
 sed -i -e 's,ta.fts.bosa.belgium.be,local.test.belgium.be,g' gui-config/config.js
 oc create configmap gui-sign-config --from-file=gui-config
 
-rm -rf gui-config
-cp -a GUI-IDP/public/config gui-config
-sed -i -e 's,ta.fts.bosa.belgium.be,local.test.belgium.be,g' gui-config/config.js
-oc create configmap gui-idp-config --from-file=gui-config
 oc create -f postgres.yaml
 oc create -f squid-whitelist.yaml
 oc process -f squid.yaml | oc create -f -
@@ -101,32 +89,24 @@ kill -TERM $PFW
 PFW=""
 oc process -f sign-validation/bosadt-openshift-project.yaml | jq -f ./jq-files/sign-validation.jq | oc create -f -
 oc process -f GUI-sign/bosadt-openshift-project.yaml | jq -f ./jq-files/gui-sign.jq | oc create -f -
-oc process -f IDP/bosadt-openshift-project.yaml | jq -f ./jq-files/idp.jq |oc create -f -
-oc process -f GUI-IDP/bosadt-openshift-project.yaml | jq -f ./jq-files/gui-idp.jq | oc create -f -
 oc process -f esealing/bosadt-openshift-project.yaml | jq -f ./jq-files/esealing.jq | oc create -f -
 status "Done; the project should now be loading into your openshift."
 echo "To access the services, edit /etc/hosts to point sign.local.test.belgium.be,"
 echo "validate.local.test.belgium.be, esealing.local.test.belgium.be,"
-echo "and idp.local.test.belgium.be to" $(minishift ip)
+echo "to" $(minishift ip)
 echo "To move on:"
 echo "  * 'minishift console' opens the OpenShift console in your default browser"
 echo "    (log on with user name 'system' and password 'admin')"
-echo ""
 echo "  * 'eval \$(minishift oc-env)' adds the 'oc' command to your shell's \$PATH"
-echo ""
 echo "  * 'oc start-build --from-dir=. -F mvn-dockers' updates the 'Dockerfile's"
 echo "    used for Java-using images"
 echo "  * For rebuilding/updating images:"
 echo "    * esealing: 'oc start-build --from-dir=./esealing -F mvn-esealing'"
-echo "    * IDP GUI: 'oc start-build --from-dir=./GUI-IDP -F guiidp'"
 echo "    * Signing GUI: 'oc start-build --from-dir=./GUI-sign -F guisign'"
-echo "    * IDP backend: 'oc start-build --from-dir=./IDP -F mvn-idp'"
 echo "    * Sign/validation backend: 'oc start-build --from-dir=./sign-validation -F mvn-signvalidation'"
 echo "    Note that a rebuild automatically triggers a restart of the (new) image"
 echo "  * 'oc get pods' lists the currently-running pods"
 echo "  * 'oc port-forward <pod name> <local port number>:<remote port number>'"
 echo "  * 'oc rsh <pod name>' gets you a shell in a pod"
-echo ""
 echo "  * 'minishift stop' stops the cluster (restart with 'minishift start')."
-echo ""
 echo "  * 'minishift delete' deletes the cluster."
